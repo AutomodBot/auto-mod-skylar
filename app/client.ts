90c47e2ded768a6a74abb9441a855f29388f65e9
Alexis Tyler <xo@wvvw.me>
2021-03-13 19:09:02 +1030

    fix: always retry after we hit a ratelimit

--- app/client.ts
+++ app/client.ts
@@ -1,9 +1,18 @@
 import Snoowrap from 'snoowrap';
 
-export const client = new Snoowrap({
+const client = new Snoowrap({
 	userAgent: 'AutoModSkylar',
 	username: process.env.USERNAME,
 	password: process.env.PASSWORD,
 	clientId: process.env.CLIENT_ID,
 	clientSecret: process.env.CLIENT_SECRET
 });
+
+// Ensure we don't throw when we hit a rate limit
+client.config({
+	continueAfterRatelimitError: true
+});
+
+export {
+	client
+};
