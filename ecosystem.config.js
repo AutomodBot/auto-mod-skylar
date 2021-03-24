8a16421f6501d629ceac1c423a55a4322d2221ab
Alexis Tyler <xo@wvvw.me>
2021-03-24 12:36:44 +1030

    chore: add deployment files and update deps

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+	apps: [
+		{
+			name: 'auto-mod-skylar',
+			script: 'npm start',
+			time: true,
+			// eslint-disable-next-line
+			append_env_to_name: true,
+			instances: 1,
+			autorestart: true,
+			// eslint-disable-next-line
+			max_restarts: 50,
+			watch: false,
+			// eslint-disable-next-line
+			max_memory_restart: '250M',
+			env: {
+				LOG_LEVEL: 'debug'
+			}
+		}
+	],
+	deploy: {
+		production: {
+			user: 'xo',
+			host: '165.227.220.113',
+			key: '~/.ssh/deploy.key',
+			ref: 'origin/main',
+			repo: 'https://github.com/automodbot/auto-mod-skylar',
+			path: '/home/xo/code/automodbot/auto-mod-skylar',
+			'pre-deploy': 'git reset --hard',
+			'post-deploy': 'npm install && npm run build && pm2 startOrGracefulReload ecosystem.config.js --env production'
+		}
+	}
+};
