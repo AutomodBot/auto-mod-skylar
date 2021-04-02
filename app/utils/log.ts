db2af5c7df1bda6b7cf2c4784be8e1de8809b6d9
Alexis Tyler <xo@wvvw.me>
2021-04-01 22:17:04 -0700

    fix: show info logs in production

--- app/utils/log.ts
+++ app/utils/log.ts
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const noop = () => {};
 
 export const log = {
-	info: isProduction ? noop : console.info,
+	info: console.info,
 	debug: isProduction ? noop : console.debug,
 	error: console.error
 };
